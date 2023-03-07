1e87bfbc178e2164a4f9bc880314f17dc1bfa50e
racejb <moonnr94@gmail.com>
2023-03-07 21:09:40 +0900

    build

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,7 +11,6 @@ module.exports = {
     "plugin:react/recommended",
     "plugin:react-hooks/recommended",
     "plugin:@typescript-eslint/recommended",
-    "next",
     "plugin:prettier/recommended",
   ],
   overrides: [],
